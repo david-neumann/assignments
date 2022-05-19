259d0a689f4e8623398c23c79d406fe3f6b3d725
David Neumann <neumann.d.david@gmail.com>
2022-05-19 13:35:16 -0500

    Completion of assignment. Items can be added, edited, and removed from shopping list.

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -17,9 +17,30 @@ form.addEventListener("submit", event => {
     li.appendChild(div);
     li.appendChild(editButton);
     li.appendChild(deleteButton);
-    
+
     // Remove item when pressing X button
     deleteButton.addEventListener("click", event => {
         event.target.parentNode.remove();
+        sessionStorage.removeItem(event.target.parentNode);
     })
+
+    // Have input box appear when Edit button is clicked
+    function editButtonClick(event) {
+        const editBox = document.createElement("input");
+        editBox.value = event.target.parentNode.childNodes[0].textContent;
+        event.target.parentNode.appendChild(editBox);
+        // Change Edit button to Save button
+        const saveButton = document.createElement("button")
+        saveButton.textContent = "Save";
+        editButton.replaceWith(saveButton);
+        // Update item on save and remove input box
+        function saveButtonClick(event) {
+            const updatedItem = editBox.value;
+            event.target.parentNode.childNodes[0].textContent = updatedItem;
+            saveButton.replaceWith(editButton);
+            editBox.remove();
+        }
+        saveButton.addEventListener("click", saveButtonClick);
+    }
+    editButton.addEventListener("click", editButtonClick);
 })
