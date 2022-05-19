747b67ee579891ebac620c3efebc80b81fac9fae
David Neumann <neumann.d.david@gmail.com>
2022-05-19 11:39:07 -0500

    Finished part two of assignment, can now remove items from shopping list

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -1,12 +1,25 @@
 const form = document.addItem; 
 
 form.addEventListener("submit", event => {
+    // Add new item to shopping list
     event.preventDefault(); 
-    const newItem = form.title.value;
+    const itemName = form.title.value;
     form.title.value = "";
+    const ul = document.getElementById("list");
     const li = document.createElement("li");
-    li.innerHTML = `<div>${newItem}</div>
-        <button>edit</button>
-        <button>X</button>`
-    document.getElementById("list").append(li);
+    const div = document.createElement("div");
+    div.textContent = itemName;
+    const editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+    ul.appendChild(li);
+    li.appendChild(div);
+    li.appendChild(editButton);
+    li.appendChild(deleteButton);
+    
+    // Remove item when pressing X button
+    deleteButton.addEventListener("click", event => {
+        event.target.parentNode.remove();
+    })
 })
