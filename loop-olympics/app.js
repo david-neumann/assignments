edbc4e7327db7fc6c54c415665a2efa407b9a4ed
David Neumann <neumann.d.david@gmail.com>
2022-05-23 15:51:34 -0500

    Initial commit. Completed preliminary challenges.

---
+++ loop-olympics/app.js
@@ -0,0 +1,27 @@
+// Preliminaries
+
+// #1 - Write a for loop that prints to the console the numbers 0 through 9.
+
+// for (let i = 0; i <= 9; i++) {
+//     console.log(i);
+// }
+
+// #2 - Write a for loop that prints to the console 9 through 0.
+
+// for (let i = 9; i >= 0; i--) {
+//     console.log(i)
+// }
+
+// #3 - Write a for loop that prints these fruits to the console.
+
+// const fruit = ["banana", "orange", "apple", "kiwi"]
+
+// for (let i = 0; i < fruit.length; i++) {
+//     console.log(fruit[i]);
+// }
+
+
+
+
+// Bronze Medal
+
