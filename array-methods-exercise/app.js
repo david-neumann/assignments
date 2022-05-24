e8af71c07992668b2c8f5dba531ddbc9d452238c
David Neumann <neumann.d.david@gmail.com>
2022-05-24 14:14:20 -0500

    Completion of assignment

--- array-methods-exercise/app.js
+++ array-methods-exercise/app.js
@@ -1,5 +1,64 @@
 var fruit = ["banana", "apple", "orange", "watermelon"];
 var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
 
-console.log("fruit: ", fruit);
-console.log("vegetables: ", vegetables);
+// 1
+
+vegetables.pop();
+
+// console.log("fruit: ", fruit);
+// console.log("vegetables: ", vegetables);
+
+// 2
+
+fruit.shift(); 
+
+// console.log("fruit: ", fruit);
+// console.log("vegetables: ", vegetables);
+
+// 3
+
+let orangeIndex = fruit.indexOf("orange");
+// console.log(orangeIndex)
+
+// 4
+
+fruit.push(orangeIndex);
+
+// console.log("fruit: ", fruit);
+// console.log("vegetables: ", vegetables);
+
+// 5
+
+let vegetablesLength = vegetables.length;
+// console.log(vegetablesLength);
+
+// 6
+
+vegetables.push(vegetablesLength);
+
+// console.log("fruit: ", fruit);
+// console.log("vegetables: ", vegetables);
+
+// 7
+
+let food = fruit.concat(vegetables);
+
+// console.log(food);
+
+// 8
+
+food.splice(4, 2);
+
+// console.log(food);
+
+// 9 
+
+food.reverse()
+
+// console.log(food);
+
+// 10
+
+let foodString = food.join(",")
+
+console.log(foodString);
