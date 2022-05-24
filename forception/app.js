597797313e2bcc6ccccadb7e327358462e29e075
David Neumann <neumann.d.david@gmail.com>
2022-05-24 16:25:05 -0500

    Completion of assignment

--- forception/app.js
+++ forception/app.js
@@ -1,2 +1,17 @@
 var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+let newArray = []; // new array to push final output to
+
+let forception = () => {
+    // loop through people array first and push names to new array one at a time
+    for (let i = 0; i < people.length; i++) {
+        newArray.push(people[i]); 
+        // between each name, loop through alphabet and push each letter to array
+        for (let j = 0; j < alphabet.length; j++) {
+            newArray.push(alphabet[j].toUpperCase())
+        }
+    } return newArray
+}
+
+console.log(forception());
