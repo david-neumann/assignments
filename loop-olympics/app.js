7380df2b999cac7c01476dc64e8fa2bbf8438f0b
David Neumann <neumann.d.david@gmail.com>
2022-05-24 12:22:48 -0500

    Completion of Bronze Medal challenges

--- loop-olympics/app.js
+++ loop-olympics/app.js
@@ -25,3 +25,36 @@
 
 // Bronze Medal
 
+// #1 - Write a for loop that will push the numbers 0 through 9 to an array.
+
+// let numArr = [];
+
+// for (let i = 0; i <= 9; i++) {
+//     numArr.push(i);
+// }
+
+// console.log(numArr);
+
+// #2 - Write a for loop that prints to the console only even numbers 0 through 100.
+
+// for (let i = 0; i <= 100; i += 2) {
+//     console.log(i);
+// }
+
+// #3 - Write a for loop that will push every other fruit to an array.
+
+// const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+
+// let newFruit = [];
+
+// for (let i = 0; i < fruit.length; i += 2) {
+//     newFruit.push(fruit[i]);
+// }
+
+// console.log(newFruit);
+
+
+
+
+// Silver Medal
+
