354d2ef86913ab8f528bd9686f231de69b173ccc
David Neumann <neumann.d.david@gmail.com>
2022-05-24 19:36:31 -0500

    Completion of assignment

--- string-methods-exercise/app.js
+++ string-methods-exercise/app.js
@@ -1,14 +1,40 @@
 // 1 - Write a function that takes a string as a parameter and returns the same string in all capital letters followed by all lowercase letters.
 
-let exampleString = "The quick brown fox jumped over the lazy dog";
+// let exampleString = "The quick brown fox jumped over the lazy dog";
+
+// let capitalizeAndLowercase = () => {
+//     let capitalString = exampleString.toUpperCase();
+//     let lowercaseString = exampleString.toLowerCase(); 
+//     let newString = capitalString.concat(lowercaseString);
+//     return newString; 
+// }
+
+// console.log(capitalizeAndLowercase(exampleString));
+
+
+// 2 - Write a function that takes a string as a parameter and returns a number that is half the string's length, rounded down.
+
+let string = "Ow now brown cow"; 
+
+let findMiddleIndex = () => Math.floor(string.length / 2); 
+
+// console.log(findMiddleIndex(string)); 
+
+
+// 3 - Write a function that uses slice() and the other functions you've written to return the first half of the given string.
+
+let returnFirstHalf = () => string.slice(0, findMiddleIndex(string))
+
+// console.log(returnFirstHalf()); 
+
+// 4 - Write a function that takes a string as a parameter and returns that string where the first half is capitalized, and the second half is lowercase.
+
+let returnSecondHalf = () => string.slice(findMiddleIndex(string))
 
 let capitalizeAndLowercase = () => {
-    let capitalString = exampleString.toUpperCase();
-    let lowercaseString = exampleString.toLowerCase(); 
-    let newString = capitalString.concat(lowercaseString);
-    return newString; 
+    let capitalFirst = returnFirstHalf().toUpperCase();
+    let lowerSecond = returnSecondHalf().toLowerCase();
+    return capitalFirst.concat(lowerSecond);
 }
 
-console.log(capitalizeAndLowercase(exampleString));
-
-
+console.log(capitalizeAndLowercase());
