438af1cb4ae980811a6b9110cdc3b11ae972c4b5
David Neumann <neumann.d.david@gmail.com>
2022-05-31 11:29:48 -0500

    Completion of assignment

--- problem-solving-practice/app.js
+++ problem-solving-practice/app.js
@@ -29,3 +29,12 @@ console.log(characterIndex("!", ["Hey!", "Wow", "Yowza!", "Whoa"]))
 
 // Write a function that takes a num1 and num2 and returns whether num1 is divisible by num2
 
+const isDivisible = (num1, num2) => {
+    if (num1 % num2 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(isDivisible(7, 2))
