a14565c827a8dbae213de3d6681bc6dfd21db4b6
David Neumann <neumann.d.david@gmail.com>
2022-06-01 15:58:11 -0500

    Game completed, all requirements satisfied

--- colossal-adventure-rpg/app.js
+++ colossal-adventure-rpg/app.js
@@ -27,9 +27,7 @@ let enemies = [
 
 const randomNumber = num1 => Math.floor(Math.random() * num1);
 
-const printDetails = () => {
-    console.log(`Hi ${userName}. Your current HP is: ${hp}. You have the following items in your inventory: ${inventory}`);
-}
+const printDetails = () => console.log(`Hi ${userName}. Your current HP is: ${hp}. You have the following items in your inventory: ${inventory}`);
 
 const doesEnemyAppear = () => {
     let enemyAppears = randomNumber(3);
@@ -42,26 +40,28 @@ const doesEnemyAppear = () => {
 
 const attack = () => {
     console.log("You've chosen to attack!");
-    let damageAmount = randomNumber(100);
     let enemyHealth = randomNumber(100);
     while (enemyHealth > 0 && hp > 0) {
+        let damageAmount = randomNumber(100);
         enemyHealth -= damageAmount; 
-        console.log(`Damage is: ${damageAmount}. Enemy health is: ${enemyHealth}.`);
         if (enemyHealth > 0) {
             console.log(`Your spell dealt a blow of ${damageAmount}. ${currentEnemy} has ${enemyHealth} HP remaining.`);
             let enemyDamageAmount = randomNumber(100);
             hp -= enemyDamageAmount; 
             console.log(`${currentEnemy} released a counterspell, dealing you ${enemyDamageAmount} damage. Your HP is: ${hp}. ${currentEnemy}'s HP is: ${enemyHealth}.`);
+            if (hp <= 0) {
+                console.log(`UNFORGIVABLE! ${userName} has been struck with ${currentEnemy}'s killing curse. GAME OVER :(`)
+                break;
+            }
         } else if (enemyHealth <= 0) {
-            console.log(`UNFORGIVABLE, your spell took out ${currentEnemy}!!!`)
+            console.log(`HOLY EXPELLIARMUS, your spell took out ${currentEnemy}!!!`)
             for (let i = 0; i < enemies.length; i++) {
                 if (currentEnemy === enemies[i].name) {
                     inventory.push(enemies[i].specialItem);
-                    let hpReward = randomNumber(100-hp);
+                    let hpReward = randomNumber(100 - hp);
                     hp += hpReward;
                     console.log(`As a reward for taking out ${currentEnemy}, you've been given a boost of ${hpReward} HP and added ${enemies[i].specialItem} to your inventory.`); 
                     enemies.splice(i, 1); 
-                    // console.log(enemies);
                     if (enemies.length === 0) {
                         console.log(`Congratulations, you've elminated all of the Death Eaters!`)
                     } else {
@@ -75,6 +75,15 @@ const attack = () => {
 
 const run = () => {
     console.log("Oh no, RUUUUUUNNNNNN!!!!");
+    let escape = randomNumber(2); 
+    if (escape === 0) {
+        hp = 0; 
+        console.log(`UNFORGIVABLE!! While attempting to flee, ${userName} was struck in the back with ${currentEnemy}'s Cruciatus Curse. The pain was excruciating and ${userName} was unable to go on. GAME OVER :(`);
+    } else {
+        let enemyDamageAmount = randomNumber(hp - 10);
+        hp -= enemyDamageAmount;
+        console.log(`${userName} manages to escape, but was struck with a glancing blow from ${currentEnemy}'s curse, taking ${enemyDamageAmount} damage. Your HP is now: ${hp}`);
+    }
 }
 
 const walk = () => {
@@ -82,7 +91,7 @@ const walk = () => {
     if (enemyPresent === true) {
         currentEnemy = enemies[Math.floor(Math.random() * enemies.length)].name;
         console.log(`DEATH EATER! You've been confronted by ${currentEnemy}!`);
-        let userSelect = readlineSync.keyIn("What would you like to do? Press (a) to cast an attack spell or (r) to run. ", {limit: 'ar'});
+        let userSelect = readlineSync.keyIn("Press (a) to cast an attack spell or (r) to run. ", {limit: 'ar'});
         if (userSelect === "a") {
             attack();
         } else if (userSelect === "r") {
@@ -102,6 +111,6 @@ const playGame = () => {
     }
 }
 
-while (hp > 0 || enemies.length !== 0) {
+while (hp > 0 && enemies.length !== 0) {
     playGame();
 }
