90945375e3c548b21e2d34e20b7b86237393ca6e
David Neumann <neumann.d.david@gmail.com>
2022-06-01 14:21:43 -0500

    Attack function is completed and working as expected

--- colossal-adventure-rpg/app.js
+++ colossal-adventure-rpg/app.js
@@ -1,6 +1,6 @@
 const readlineSync = require('readline-sync');
 const userName = readlineSync.question('May I have your name? ');
-console.log(`Hi ${userName}, welcome to the Colossal Adventure RPG!`)
+console.log(`Hi ${userName}, welcome to the Colossal Harry Potter RPG!`)
 
 let hp = 100;
 let inventory = [];
@@ -20,34 +20,19 @@ let enemies = [
         specialItem: "Salazar Slytherin's locket"
     },
     {
-        name: "Dementor",
+        name: "Bellatrix Lestrange",
         specialItem: "Stag patronus"
     }
 ];
 
+const randomNumber = num1 => Math.floor(Math.random() * num1);
 
 const printDetails = () => {
     console.log(`Hi ${userName}. Your current HP is: ${hp}. You have the following items in your inventory: ${inventory}`);
 }
 
-const walk = () => {
-    doesEnemyAppear();
-    if (enemyPresent === true) {
-        currentEnemy = enemies[Math.floor(Math.random() * enemies.length)].name;
-        console.log(`You've been attacked by ${currentEnemy}!`);
-        let userSelect = readlineSync.keyIn("What would you like to do? Press (a) to attack or (r) to run. ", {limit: 'ar'});
-        if (userSelect === "a") {
-            attack();
-        } else if (userSelect === "r") {
-            run();
-        }
-    } else {
-        console.log("No enemies found yet. Keep walking.")
-    }
-}
-
 const doesEnemyAppear = () => {
-    let enemyAppears = Math.floor(Math.random() * 3);
+    let enemyAppears = randomNumber(3);
     if (enemyAppears === 0) {
         return enemyPresent = true;
     } else {
@@ -57,22 +42,32 @@ const doesEnemyAppear = () => {
 
 const attack = () => {
     console.log("You've chosen to attack!");
-    let damageAmount = Math.floor(Math.random() * 100);
-    let enemyHealth = Math.floor(Math.random() * 100);
-    enemyHealth -= damageAmount; 
-    if (enemyHealth > 0) {
-        console.log(`Your attack dealt a blow of ${damageAmount}. ${currentEnemy} has ${enemyHealth} HP remaining.`);
-        let enemyDamageAmount = Math.floor(Math.random() * 100);
-        hp -= enemyDamageAmount; 
-        console.log(`Your HP is: ${hp}`);
-    } else if (enemyHealth <= 0) {
-        console.log(`WICKED ATTACK, you've killed ${currentEnemy}!!!`)
-        for (let i = 0; i < enemies.length; i++) {
-            if (currentEnemy === enemies[i].name) {
-                inventory.push(enemies[i].specialItem);
-                console.log(`As a reward for killing ${currentEnemy}, you've added ${enemies[i].specialItem} to your inventory.`); 
-                enemies.splice(i, 1); 
-                console.log(enemies);
+    let damageAmount = randomNumber(100);
+    let enemyHealth = randomNumber(100);
+    while (enemyHealth > 0 && hp > 0) {
+        enemyHealth -= damageAmount; 
+        console.log(`Damage is: ${damageAmount}. Enemy health is: ${enemyHealth}.`);
+        if (enemyHealth > 0) {
+            console.log(`Your spell dealt a blow of ${damageAmount}. ${currentEnemy} has ${enemyHealth} HP remaining.`);
+            let enemyDamageAmount = randomNumber(100);
+            hp -= enemyDamageAmount; 
+            console.log(`${currentEnemy} released a counterspell, dealing you ${enemyDamageAmount} damage. Your HP is: ${hp}. ${currentEnemy}'s HP is: ${enemyHealth}.`);
+        } else if (enemyHealth <= 0) {
+            console.log(`UNFORGIVABLE, your spell took out ${currentEnemy}!!!`)
+            for (let i = 0; i < enemies.length; i++) {
+                if (currentEnemy === enemies[i].name) {
+                    inventory.push(enemies[i].specialItem);
+                    let hpReward = randomNumber(100-hp);
+                    hp += hpReward;
+                    console.log(`As a reward for taking out ${currentEnemy}, you've been given a boost of ${hpReward} HP and added ${enemies[i].specialItem} to your inventory.`); 
+                    enemies.splice(i, 1); 
+                    // console.log(enemies);
+                    if (enemies.length === 0) {
+                        console.log(`Congratulations, you've elminated all of the Death Eaters!`)
+                    } else {
+                        console.log(`Stay vigilant, there are still ${enemies.length} Death Eathers lurking.`)
+                    }
+                }
             }
         }
     }
@@ -80,6 +75,22 @@ const attack = () => {
 
 const run = () => {
     console.log("Oh no, RUUUUUUNNNNNN!!!!");
+}
+
+const walk = () => {
+    doesEnemyAppear();
+    if (enemyPresent === true) {
+        currentEnemy = enemies[Math.floor(Math.random() * enemies.length)].name;
+        console.log(`DEATH EATER! You've been confronted by ${currentEnemy}!`);
+        let userSelect = readlineSync.keyIn("What would you like to do? Press (a) to cast an attack spell or (r) to run. ", {limit: 'ar'});
+        if (userSelect === "a") {
+            attack();
+        } else if (userSelect === "r") {
+            run();
+        }
+    } else {
+        console.log("No Death Eaters found yet. Keep walking.")
+    }
 }
 
 const playGame = () => {
@@ -91,6 +102,6 @@ const playGame = () => {
     }
 }
 
-while (hp > 0) {
+while (hp > 0 || enemies.length !== 0) {
     playGame();
 }
