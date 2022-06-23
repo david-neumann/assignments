9172292989651e168b1c4be163460feca86e9336
David Neumann <neumann.d.david@gmail.com>
2022-06-23 13:29:20 -0500

    Initial commit

---
+++ princess-in-another-castle/app.js
@@ -0,0 +1,28 @@
+class Player {
+    constructor (name, totalCoins, status, hasStar) {
+        this.name = name;
+        this.totalCoins = totalCoins; 
+        this.status = status; 
+        this.hasStar = hasStar; 
+    }
+
+    setName() {
+
+    }
+
+    gotHit() {
+
+    }
+
+    gotPowerup() {
+
+    }
+
+    addCoin() {
+
+    }
+
+    print() {
+        
+    }
+}
