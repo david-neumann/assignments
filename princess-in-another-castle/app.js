21c335b07174b0bc59c6d9ac7f32fa5f8d257b9a
David Neumann <neumann.d.david@gmail.com>
2022-06-24 11:14:59 -0500

    Completion of assignment

--- princess-in-another-castle/app.js
+++ princess-in-another-castle/app.js
@@ -6,23 +6,63 @@ class Player {
         this.hasStar = hasStar; 
     }
 
-    setName() {
-
+    setName(namePicked) {
+        if (namePicked === 'Mario' || namePicked === 'Luigi') {
+            this.name = namePicked;
+        }
     }
 
     gotHit() {
-
+        if (this.status === 'Powered Up') {
+            this.status = 'Big';
+        } else if (this.status === 'Big') {
+            this.status = 'Small';
+        } else if (this.status === 'Small') {
+            this.status = 'Dead';
+        }
     }
 
     gotPowerup() {
-
+        if (this.status === 'Small') {
+            this.status = 'Big';
+        } else if (this.status === 'Big') {
+            this.status = 'Powered Up';
+        } else if (this.status === 'Powered Up') {
+            this.hasStar = true;
+        }
     }
 
     addCoin() {
-
+        this.totalCoins++;
     }
 
     print() {
-        
+        if (this.hasStar) {
+            console.log(`Name: ${this.name}\nStatus: ${this.status}\nTotal Coins: ${this.totalCoins}\nYou have a star\n`);
+        }
+        console.log(`Name: ${this.name}\nStatus: ${this.status}\nTotal Coins: ${this.totalCoins}\n`)
     }
 }
+
+const mario = new Player ('Mario', 0, 'Small', false);
+
+let intervalId;
+
+const randomNumber = () => Math.floor(Math.random() * 3)
+
+const playGame = () => {
+    let randomFunction = randomNumber();
+    if (randomFunction === 0) {
+        mario.gotHit();
+    } else if (randomFunction === 1) {
+        mario.gotPowerup();
+    } else if (randomFunction === 2) {
+        mario.addCoin();
+    }
+    mario.print();
+    if (mario.status === 'Dead') {
+        clearInterval(intervalId)
+    }
+}
+
+intervalId = setInterval(playGame, 1000);
