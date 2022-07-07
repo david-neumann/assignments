643506b108bd034940974372951a790484f94d3b
David Neumann <neumann.d.david@gmail.com>
2022-07-07 15:32:19 -0500

    Main body styled. Project complete.

--- _React/info-site/src/App.js
+++ _React/info-site/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import "./styles.css";
 import Navbar from './components/Navbar';
 import Main from './components/Main';
--- _React/info-site/src/components/Main.js
+++ _React/info-site/src/components/Main.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export default function Main() {
     return (
-        <h1>Main content goes here</h1>
+        <main>
+            <h1 className='main__header'>Fun facts about React</h1>
+            <ul className='main__fact-list'>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
