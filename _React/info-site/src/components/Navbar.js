77dc39c0684e00568eaccf1e34250fa4396fe515
David Neumann <neumann.d.david@gmail.com>
2022-07-07 14:41:16 -0500

    Completion of nav bar styling

--- _React/info-site/src/components/Navbar.js
+++ _React/info-site/src/components/Navbar.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import reactLogo from '../images/reactjs-icon.png';
 
 export default function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <nav>
+            <img src={reactLogo} className='nav__logo' />
+            <h3 className='nav__logo-text'>ReactFacts</h3>
+            <h4 className='nav__title'>React Course - Project 1</h4>
+        </nav>
     )
 }
