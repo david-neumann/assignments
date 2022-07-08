9e50450b1d7406d6f6dc875d8eee95023175c69b
David Neumann <neumann.d.david@gmail.com>
2022-07-08 10:32:47 -0500

    Footer component finished. Project completed.

--- _React/digital-business-card/src/components/About.js
+++ _React/digital-business-card/src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <>
             <h2 className='main--heading'>About</h2>
-            <p className='main--text'>I'm a full-stack web developer with a passion for making beautiful and simple to use web apps. I love learning new things and solving challenging problems.</p>
+            <p className='main--text'>I'm a full stack web developer with a passion for making beautiful and simple to use web apps. I love learning new things and solving challenging problems.</p>
         </>
     )
 }
--- _React/digital-business-card/src/components/Footer.js
+++ _React/digital-business-card/src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import twitter from '../images/twitter-icon.png'
+import facebook from '../images/facebook-icon.png'
+import instagram from '../images/instagram-icon.png'
+import github from '../images/github-icon.png'
 
 export default function Footer() {
     return (
         <footer>
-            Social icons here
+            <a href=''><img src={twitter} className='social-icon' /></a>
+            <a href=''><img src={facebook} className='social-icon' /></a>
+            <a href=''><img src={instagram} className='social-icon' /></a>
+            <a href=''><img src={github} className='social-icon' /></a>
         </footer>
     )
 }
