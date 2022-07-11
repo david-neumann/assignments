08a1831defc75ed0334cf4397286176bae70af74
David Neumann <neumann.d.david@gmail.com>
2022-07-11 15:55:24 -0500

    Card component completed. All styling done. Project completed.

--- _React/travel-journal/src/components/Card.js
+++ _React/travel-journal/src/components/Card.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-export default function Card() {
+export default function Card(props) {
     return (
-        <></>
+        <section className='card'>
+            <figure className='card--image'>
+                <img src={props.imageUrl} className='card--photo'/>
+            </figure>
+            <div className='card--info'>
+                <div className='card--location'>
+                    <img src='/images/location-marker.png' className='card--location--marker'/>
+                    <h4 className='card--location--state'>{props.location}</h4>
+                    <a href={props.googleMapsUrl} >View on Google Maps</a>
+                </div>
+                <h1 className='card--title'>{props.title}</h1>
+                <h3 className='card--dates'>{`${props.startDate} - ${props.endDate}`}</h3>
+                <p className='card--desc'>{props.description}</p>
+            </div>
+        </section>
     )
 }
--- _React/travel-journal/src/data.js
+++ _React/travel-journal/src/data.js
@@ -5,8 +5,8 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/QoqYB6WL2mGjoaZK7",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
-        description: "Glacier National Park is a 1,583-sq.-mi. wilderness area in Montana's Rocky Mountains, with glacier-carved peaks and valleys running to the Canadian border. It's crossed by the mountainous Going-to-the-Sun Road. Among more than 700 miles of hiking trails, it has a route to photogenic Hidden Lake. Other activities include backpacking, cycling and camping. Diverse wildlife ranges from mountain goats to grizzly bears.",
-        imageUrl: "https://unsplash.com/photos/2cB599inAew"
+        description: "Glacier National Park is a 1,583-sq.-mi. wilderness area in Montana's Rocky Mountains, with glacier-carved peaks and valleys running to the Canadian border.",
+        imageUrl: "/images/glacier.jpg"
     },
     {
         title: "Grand Teton National Park",
@@ -14,8 +14,8 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/QrBVwfvQxn75broc6",
         startDate: "27 May, 2021",
         endDate: "8 Jun, 2021",
-        description: "Rising above a scene rich with extraordinary wildlife, pristine lakes, and alpine terrain, the Teton Range stands as a monument to the people who fought to protect it. These are mountains of the imagination. Mountains that led to the creation of Grand Teton National Park where you can explore over two hundred miles of trails, float the Snake River, and enjoy the serenity of this remarkable place.",
-        imageUrl: "https://unsplash.com/photos/edY8ngg3sq4"
+        description: "Rising above a scene rich with extraordinary wildlife, pristine lakes, and alpine terrain, the Teton Range stands as a monument to the people who fought to protect it. These are mountains of the imagination.",
+        imageUrl: "/images/grand-teton.jpg"
     },
     {
         title: "Zion National Park",
@@ -23,7 +23,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/CnU4wV6KrU6U9ZeX8",
         startDate: "01 Oct, 2021",
         endDate: "18 Nov, 2021",
-        description: "Zion National Park is a southwest Utah nature preserve distinguished by Zion Canyon’s steep red cliffs. Zion Canyon Scenic Drive cuts through its main section, leading to forest trails along the Virgin River. The river flows to the Emerald Pools, which have waterfalls and a hanging garden. Also along the river, partly through deep chasms, is Zion Narrows wading hike.",
-        imageUrl: "https://unsplash.com/photos/aHdAdA0JzLE"
+        description: "Zion National Park is a southwest Utah nature preserve distinguished by Zion Canyon’s steep red cliffs. Zion Canyon Scenic Drive cuts through its main section, leading to forest trails along the Virgin River. ",
+        imageUrl: "/images/zion.jpg"
     }
 ]
