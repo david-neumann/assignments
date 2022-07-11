1a51dbd5b381f62e182c1ea436c4cd6faea84af6
David Neumann <neumann.d.david@gmail.com>
2022-07-11 14:18:38 -0500

    Initial project setup.

---
+++ _React/travel-journal/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Nav from "./components/Nav";
+import Card from "./components/Card";
+import data from "./data";
+
+export default function App() {
+    return (
+        <></>
+    )
+}
