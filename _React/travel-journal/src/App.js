37ff82991ea120fbae4814ce5439d5cf5e4e8726
David Neumann <neumann.d.david@gmail.com>
2022-07-11 14:40:11 -0500

    Nav component completed.

--- _React/travel-journal/src/App.js
+++ _React/travel-journal/src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
-import Nav from "./components/Nav";
-import Card from "./components/Card";
-import data from "./data";
+import React from 'react';
+import Nav from './components/Nav';
+import Card from './components/Card';
+import data from './data';
 
 export default function App() {
+    const cards = data.map(card => <Card {...card}/>)
+    
     return (
-        <></>
+        <div className='container' >
+            <Nav />
+            {cards}
+        </div>
     )
 }
--- _React/travel-journal/src/components/Nav.js
+++ _React/travel-journal/src/components/Nav.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function Nav() {
     return (
-        <></>
+        <nav>
+            <img src='/images/world-logo.png' className='nav--logo'/>
+            <h2>my travel journal.</h2>
+        </nav>
     )
 }
