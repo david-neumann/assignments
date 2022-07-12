4046a2531eb34e5235d9dc5400c6ce8b21b0a63c
David Neumann <neumann.d.david@gmail.com>
2022-07-12 17:14:46 -0500

    Passing props into Card component. Need to finish styling.

--- _React/vacation-spots/src/components/Card.js
+++ _React/vacation-spots/src/components/Card.js
@@ -12,7 +12,9 @@ export default function Card(props) {
 
     return (
         <section className='card' style={{backgroundImage: `url(${props.imgUrl})`}}>
-
+            {badgeText && <div className='card--badge'>{badgeText}</div>}
+            <h1 className='card--place'>{props.place}</h1>
+            <p className='card--text'>Best time of the year to go: {props.timeToGo}</p>
         </section>
     )
 }
