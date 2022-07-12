6713a2925e63685b410741381b3879eac8d79274
David Neumann <neumann.d.david@gmail.com>
2022-07-12 13:42:46 -0500

    BlogPost component completed and populating in BlogList component

--- _React/blog-props/src/components/BlogList.js
+++ _React/blog-props/src/components/BlogList.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import BlogPost from './BlogPost';
+import data from '../data';
 
 export default function BlogList() {
+    const blogPosts = data.map(post => <BlogPost {...post} />)
+    
     return (
-        <></>
+        <main>
+            <div className='blog-list'>
+                {blogPosts}
+                <div className='button--older-posts'>
+                    <button>Older Posts →</button>
+                </div>
+            </div>
+        </main>
     )
 }
--- _React/blog-props/src/components/BlogPost.js
+++ _React/blog-props/src/components/BlogPost.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-export default function BlogPost() {
+export default function BlogPost(props) {
     return (
-        <></>
+        <section className='blog-post'>
+            <a href=''>
+                <h2 className='blog-post--title'>{props.title}</h2>
+                <h4 className='blog-post--subtitle'>{props.subTitle}</h4>
+            </a>
+            <p className='blog-post--meta'>
+                Posted by <a href='' className='blog-post--meta-author'>{props.author}</a> on {props.date}
+            </p>
+        </section>
     )
 }
