5790b07def751f05052dca65f91473e6343cecaf
David Neumann <neumann.d.david@gmail.com>
2022-07-12 14:44:36 -0500

    Footer component completed. Project completed.

--- _React/blog-props/src/components/Footer.js
+++ _React/blog-props/src/components/Footer.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <></>
+        <footer>
+            <div className='footer--social-icons'>
+                <a href=''><img src='/images/twitter.png' className='social-icon' /></a>
+                <a href=''><img src='/images/facebook.png' className='social-icon' /></a>
+                <a href=''><img src='/images/tiktok.png' className='social-icon' /></a>
+            </div>
+            <p className='footer--copyright'>Copyright © Your Website 2022</p>
+        </footer>
     )
 }
