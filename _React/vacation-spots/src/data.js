76bb9579fe0ca47d1239b06ea01d14bc94f600b1
David Neumann <neumann.d.david@gmail.com>
2022-07-12 15:41:06 -0500

    Initial project setup

---
+++ _React/vacation-spots/src/data.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        place: "New Zealand",
+        price: 1800,
+        timeToGo: "Summer"
+    },
+    {
+        place: "Amalfi Coast, Italy",
+        price: 1700,
+        timeToGo: "Spring"
+    },
+    {
+        place: "Glacier National Park",
+        price: 600,
+        timeToGo: "Summer"
+    },
+    {
+        place: "Iceland",
+        price: 1200,
+        timeToGo: "Fall"
+    },
+    {
+        place: "Big Bend National Park",
+        price: 100,
+        timeToGo: "Winter"
+    }
+]
