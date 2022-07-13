8b76ef9ae969c256a8e3ff406092528ee1085ed0
David Neumann <neumann.d.david@gmail.com>
2022-07-13 15:25:33 -0500

    Header component completed.

--- _React/meme-generator/src/App.jsx
+++ _React/meme-generator/src/App.jsx
@@ -3,6 +3,8 @@ import Header from './components/Header';
 
 export default function App() {
   return (
-    <></>
+    <div className='container'>
+      <Header />
+    </div>
   )
 }
--- _React/meme-generator/src/components/Header.jsx
+++ _React/meme-generator/src/components/Header.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function Header() {
     return (
-        <></>
+        <header>
+            <div className='header--logo'>
+                <img src='/images/troll-face.png' className='header--icon'/>
+                <h2 className='header--title'>Meme Generator</h2>
+            </div>
+            <h4 className='header--info'>React Course - Project 3</h4>
+        </header>
     )
 }
