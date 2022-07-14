fe05408898f7ee28c4cd436ee8cb24a14b1d99aa
David Neumann <neumann.d.david@gmail.com>
2022-07-14 18:02:21 -0500

    Completion of assignment

--- _React/dj-react/src/App.jsx
+++ _React/dj-react/src/App.jsx
@@ -24,11 +24,59 @@ export default function App() {
     })
   }
 
+  const bottomLftClick = () => {
+    setColorsArr(prevState => {
+      const newColorsArr = prevState.map((color, index) => index === 2 ? color = 'blue' : color)
+      return [...newColorsArr];
+    })
+  }
+
+  const bottomRgtClick = () => {
+    setColorsArr(prevState => {
+      const newColorsArr = prevState.map((color, index) => index === 3 ? color = 'blue' : color)
+      return [...newColorsArr];
+    })
+  }
+
+  const bigDjOneClick = () => {
+    setColorsArr(prevState => {
+      const newColorsArr = prevState.map((color, index) => index === 0 ? color = 'firebrick' : color)
+      return [...newColorsArr];
+    })
+  }
+
+  const bigDjTwoClick = () => {
+    setColorsArr(prevState => {
+      const newColorsArr = prevState.map((color, index) => index === 1 ? color = 'coral' : color)
+      return [...newColorsArr];
+    })
+  }
+
+  const bigDjThreeClick = () => {
+    setColorsArr(prevState => {
+      const newColorsArr = prevState.map((color, index) => index === 2 ? color = 'green' : color)
+      return [...newColorsArr];
+    })
+  }
+
+  const bigDjFourClick = () => {
+    setColorsArr(prevState => {
+      const newColorsArr = prevState.map((color, index) => index === 3 ? color = 'yellow' : color)
+      return [...newColorsArr];
+    })
+  }
+
   return (
     <main>
       {squareColors}
       <button onClick={smallClick}>Small DJ</button>
       <button onClick={partyClick}>Party DJ</button>
+      <button onClick={bottomLftClick}>Bottom Left</button>
+      <button onClick={bottomRgtClick}>Bottom Right</button>
+      <button onClick={bigDjOneClick}>Big DJ - 1</button>
+      <button onClick={bigDjTwoClick}>Big DJ - 2</button>
+      <button onClick={bigDjThreeClick}>Big DJ - 3</button>
+      <button onClick={bigDjFourClick}>Big DJ - 4</button>
     </main>
   )
 }
