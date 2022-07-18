673851c68434b51e654b547bb5a88a887b951534
David Neumann <neumann.d.david@gmail.com>
2022-07-18 13:45:22 -0500

    Completion of project

--- _React/name-entry/src/App.jsx
+++ _React/name-entry/src/App.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function App() {
+  const [firstName, setFirstName] = useState('');
+  const [nameList, setNameList] = useState([]);
+
+  const handleChange = (event) => {
+    const {value} = event.target;
+    setFirstName(value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setNameList(prevState => [...prevState, firstName]);
+  }
+
+  const createListElements = nameList.map(name => <li>{name}</li>);
+  
   return (
-    <></>
+    <div>
+      <h1>{firstName}</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type='text'
+          placeholder='First Name'
+          name='firstName'
+          onChange={handleChange}
+        /> 
+        <br />
+        <button>Add to list</button>
+      </form>
+      <ol>
+        {createListElements}
+      </ol>
+    </div>
   )
 }
