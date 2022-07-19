1da4f29c6f9833b9d74546367687e7f8ab5f23b4
David Neumann <neumann.d.david@gmail.com>
2022-07-19 14:45:10 -0500

    Project completed

--- _React/random-color-component/src/RandomColor.jsx
+++ _React/random-color-component/src/RandomColor.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function RandomColor() {
-    return (
-        <div className='color-box'>
+    const [randomColor, setRandomColor] = useState();
 
+    useEffect(() => {
+        axios.get(`https://www.colr.org/json/color/random?timestamp=${new Date().getTime()}`)
+            .then(res => setRandomColor(res.data.colors[0].hex))
+            .catch(err => console.log(err))
+    }, [])
+    
+    return (
+        <div>
+            <div 
+                className='color-box'
+                style={{backgroundColor: `#${randomColor}`}}
+            >
+    
+            </div>
         </div>
     )
 }
